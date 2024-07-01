5296ef016f534897502cd730700185565130144c
yaid711 <yazid.andegesten@gmail.com>
2024-07-01 15:08:28 +0700

    fix layout user

--- resources/views/userPage.blade.php
+++ resources/views/userPage.blade.php
@@ -1,12 +1,16 @@
 @extends('layout.layout')
 
 @section('content')
-<div class="container">
-    <h1>Welcome, {{ $user->name }}</h1>
-    <p>Email: {{ $user->email }}</p>
-    <p>Username: {{ $user->username }}</p> 
-    <p>Booth Booked: {{ $user->booth_booked }}</p> 
-
-    <a href="{{ url('logout') }}" class="bg-red-500 p-5 rounded-lg">Logout</a>
+<div class="container mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg relative">
+    <div class="absolute top-0 right-0 m-6">
+        <a href="{{ url('logout') }}" class="bg-red-500 text-white p-3 rounded-lg hover:bg-red-600 transition duration-300">Logout</a>
+    </div>
+    <h1 class="text-4xl font-bold mb-4">Welcome, {{ $user->name }}</h1>
+    <div class="text-lg mb-4">
+        <p><span class="font-semibold">Email:</span> {{ $user->email }}</p>
+        <p><span class="font-semibold">Username:</span> {{ $user->username }}</p> 
+        <p><span class="font-semibold">Booth Booked:</span> {{ $user->booth_booked }}</p> 
+    </div>
+    <a href="{{ url('print-pdf') }}" class="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300">Print PDF</a>
 </div>
 @endsection
