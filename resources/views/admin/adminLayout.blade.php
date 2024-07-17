4e472a1c290ec8375aa8f680872c5e8e7cb59703
yaid711 <yazid.andegesten@gmail.com>
2024-07-17 13:22:39 +0700

    admin

--- resources/views/admin/adminLayout.blade.php
+++ resources/views/admin/adminLayout.blade.php
@@ -14,7 +14,7 @@
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">Booth Booking</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            @if(Auth::check() && Auth::user()->role == 'admin')
+            @if(Auth::check() && Auth::user()-> level == 'admin')
              <a href="{{ route('admin.dashboard') }}" class="text-white bg-secondary hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Dashboard Admin</a>
               @else
              <a href="/authPage" class="text-white bg-secondary hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</a>
