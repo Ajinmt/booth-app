3a189555c1863c37d628d33b5db27c0ae32e43d1
yaid711 <yazid.andegesten@gmail.com>
2024-07-17 12:26:02 +0700

    mmerubah login user & menambah pesan

--- resources/views/admin/adminLayout.blade.php
+++ resources/views/admin/adminLayout.blade.php
@@ -8,19 +8,44 @@
     <title>{{ config('app.name') }} - Admin</title>
 </head>
 <body>
-    <nav class="p-6 bg-black text-white flex justify-between items-center">
-        <h1 class="text-3xl">Booth Booking</h1>
-        <div >
-          <ul>
-            <li class="flex gap-5">
-                <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                <a  href="{{ route('booth-management.index') }}">Booth Management</a>
-                <a href="{{ route('user-management.index') }}">User Management</a>
-                <a href="{{ url('logout') }}">Logout</a>
-            </li>
-          </ul>
+    <nav class="bg-primary dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 p-4">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">Booth Booking</span>
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            @if(Auth::check() && Auth::user()->role == 'admin')
+             <a href="{{ route('admin.dashboard') }}" class="text-white bg-secondary hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Dashboard Admin</a>
+              @else
+             <a href="/authPage" class="text-white bg-secondary hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</a>
+            @endif
+
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-primary dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="{{ route('admin.dashboard') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('booth-management.index') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:text-white">Booth Management</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('user-management.index') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:text-white">User Management</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('logout') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-accent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:text-white">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-  </nav>
+    </nav>
+
     @yield('content')
 
 </body>
