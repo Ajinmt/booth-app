147ec795c4965d70ac9f2928f649c982ae658c37
arya05 <aryawahyuaw77@gmail.com>
2024-07-18 15:46:08 +0700

    [features]update

--- resources/views/orderPage.blade.php
+++ resources/views/orderPage.blade.php
@@ -1,33 +1,51 @@
 @extends('layout.layout')
 
 @section('content')
-<h1 class="text-center md:text-3xl mb-5">Booth Tersedia</h1>
 
-<div class="grid grid-cols-2 md:grid-cols-4 place-items-center md:px-10 md:mt-10 mt-14">
-    @foreach ($booths as $booth)
-        <div class="w-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:py-6 py-4 mb-3">
+<?php
+function shortenText($text, $wordLimit) {
+    $words = explode(' ', $text);
+    if (count($words) > $wordLimit) {
+        $words = array_slice($words, 0, $wordLimit);
+        return implode(' ', $words) . ' (baca selengkapnya)';
+    }
+    return $text;
+}
+?>
+<div class="mt-20">
+    <h1 class="text-center md:text-3xl mb-5">Booth Tersedia</h1>
+    
+    <div class="grid grid-cols-2 md:grid-cols-4 place-items-center md:px-10 md:mt-10">
+        @foreach ($booths as $booth)
+        <div class="w-72 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 md:py-6 py-4 mb-3"> 
             <a href="#">
                 <h1 class="md:text-7xl text-3xl text-center font-bold">{{ $booth->nama }}</h1>
             </a>
-            <div class="md:px-5 md:pb-5 ">
-                    <h5 class="md:text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $booth->deskripsi }}</h5>
-                <div class="flex items-center justify-between gap-2">
+            <div class="md:px-2">
+                <div class="w-3/4">
+                    <h5 class="md:text-md tracking-tight text-gray-900 dark:text-white">
+                        <?php echo shortenText($booth->deskripsi, 5); ?>
+                    </h5>
+                </div>
+                <div class="flex items-center justify-between ">
                     <span class="md:text-2xl font-bold text-gray-900 dark:text-white">Rp.{{ number_format($booth->harga, 2, ',', '.') }}</span>
-                    @if(auth()->check())
-                        <a  class="text-white bg-secondary  hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "  data-modal-target="default-modal" data-modal-toggle="default-modal" >Pesan</a>
+                    @if ($booth->status == 'tersedia')
+                        @if(auth()->check())
+                            <a class="text-white bg-secondary hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer" data-modal-toggle="default-modal">Pesan</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-white bg-secondary hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer">Pesan</a>
+                        @endif
                     @else
-                        <a href="{{ route('login') }}" class="text-white bg-secondary hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " >Login</a>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Tidak Tersedia</button>
                     @endif
                 </div>
             </div>
         </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
 
-
-
 <!-- Modal -->
-<!-- Main modal -->
 <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
@@ -35,7 +53,7 @@
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Terms of Service
+                    Detail Pemesanan 
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
--- resources/views/userPage.blade.php
+++ resources/views/userPage.blade.php
@@ -1,16 +1,32 @@
 @extends('layout.layout')
 
 @section('content')
-<div class="container mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg relative">
+<!-- <div class="container mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg relative">
     <div class="absolute top-0 right-0 m-6">
         <a href="{{ url('logout') }}" class="bg-red-500 text-white p-3 rounded-lg hover:bg-red-600 transition duration-300">Logout</a>
     </div>
     <h1 class="text-4xl font-bold mb-4">Welcome, {{ $user->name }}</h1>
     <div class="text-lg mb-4">
-        <p><span class="font-semibold">Email:</span> {{ $user->email }}</p>
+        <p><span class="font-semibold">Email:</span>    </p>
         <p><span class="font-semibold">Username:</span> {{ $user->username }}</p> 
         <p><span class="font-semibold">Booth Booked:</span> {{ $user->booth_booked }}</p> 
     </div>
     <a href="{{ url('print-pdf') }}" class="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300">Print PDF</a>
+</div> -->
+<div class=" h-screen flex justify-center items-center flex-col">
+    <h1 class="text-4xl font-bold mb-4">Welcome, {{ $user->name }}</h1>
+    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex flex-col items-center py-10">
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"> {{ $user->name }}</h5>
+            <p class="text-sm text-gray-500 dark:text-gray-400"> {{ $user->username }}</p>
+            <p class="text-sm text-gray-500 dark:text-gray-400"> {{ $user->email }}</p>
+            <div class="flex mt-4 md:mt-6 gap-8">
+                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-secondary hover:opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Print PDF</a>
+                <a href="{{ url('logout') }}" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 ">logout</a>
+            </div>
+        </div>
+    </div>
 </div>
+
 @endsection
