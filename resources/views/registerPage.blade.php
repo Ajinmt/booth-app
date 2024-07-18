5cfb86ec9acd8b0cacb0ef3079be8c3b25968998
arya05 <aryawahyuaw77@gmail.com>
2024-07-18 15:44:39 +0700

    [ui]update authPage

--- resources/views/registerPage.blade.php
+++ resources/views/registerPage.blade.php
@@ -13,13 +13,13 @@
                         <span class="text-red-500 text-sm">* {{ $errors->first('name') }}</span>
                     @endif
                 </div>
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label class="block text-sm font-medium text-gray-700" for="inputusername">Username</label>
                     <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-3" id="inputusername" type="text" name="username" placeholder="Masukkan Username" />
                     @if ($errors->has('username'))
                         <span class="text-red-500 text-sm">* {{ $errors->first('username') }}</span>
                     @endif
-                </div>
+                </div> -->
                 <div class="form-group">
                     <label class="block text-sm font-medium text-gray-700" for="inputEmailAddress">Email</label>
                     <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-3" id="inputEmailAddress" type="email" name="email" placeholder="Masukkan Email" />
@@ -37,10 +37,17 @@
                 <div class="form-group">
                     <button class="w-full bg-secondary text-white py-2 px-4 rounded-xl hover:opacity-80 transition duration-200" type="submit">Daftar!</button>
                 </div>
+                <div>
+                <p class="text-center mb-3">Atau</p>
+                    <div>
+                        <button class="outline  outline-1 outline-secondary  hover:bg-opacity-80  font-medium text-sm py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline w-full flex justify-center items-center focus:outline-secondary focus:outline " type="submit"> <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="">Register Menggunakan Google</button>
+                        </div>
+                </div>
             </form>
             <div class="mt-4 text-center">
                 <a class=" hover:underline" href="{{ route('login') }}">Sudah Punya Akun? <strong>Login !</strong></a>
             </div>
+             
         </div>
     </div>
 </div>
